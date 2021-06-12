7c178a75e3079619999e2acb88c8139ce17ada32
euledge <hitoshi.kuroyanagi@gmail.com>
2021-06-12 10:15:29 +0900

    CI用調整

--- test/e2e/index.spec.js
+++ test/e2e/index.spec.js
@@ -6,9 +6,12 @@ describe('Index page', () => {
 
   beforeAll(async () => {
     jest.setTimeout(90000)
-    browser = await puppeteer.launch({ args: ['--lang=ja'], timeout: 90000 })
+    browser = await puppeteer.launch({ args: ['--lang=ja'] })
     page = await browser.newPage()
-    await page.goto('http://127.0.0.1:3000')
+    await page.goto('http://127.0.0.1:3000', {
+      waitUntil: 'load',
+      timeout: 0
+    })
   })
 
   afterAll(async () => {
